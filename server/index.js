a5d66031224aea2dd1831b427aa42bfb7595e3ab
MOHSIN YOUNAS <MOHSINYOUNAS05@GMAIL.COM>
2017-12-30 03:29:52 +0100

    React component Created

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware'
+import webpackconfig from '../webpack.config.dev.js';
+
 let app = express();
 
+app.use(webpackMiddleware(webpack(webpackconfig)));
+
+
 app.get('/*',(req,res)=>{
-  
+
   res.sendFile(path.join(__dirname,'./index.html'));
 });
 
